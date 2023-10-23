c0ce4694e759a7b5facac3fcc79f9bba5093c322
ImperialBreakCode <eeeda522@gmail.com>
2023-10-23 19:32:05 +0300

    accounts - assigning port, adding wallet entity and user repo

---
+++ src/Accounts/API.Accounts.Domain/Entities/Wallet.cs
@@ -0,0 +1,10 @@
+﻿using API.Accounts.Domain.Interfaces;
+
+namespace API.Accounts.Domain.Entities
+{
+    public class Wallet : IEntity
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+    }
+}
---
+++ src/Accounts/API.Accounts.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,13 @@
+﻿using API.Accounts.Domain.Entities;
+using API.Accounts.Domain.Interfaces;
+
+namespace API.Accounts.Infrastructure.Repositories
+{
+    public class UserRepository : Repository<User>, IUserRepository
+    {
+        public User GetOneByUserName(int username)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
