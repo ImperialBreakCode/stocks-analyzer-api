c37ac6d9b5a8d877d5a58cd2aa02836b15352fdd
1243557 <steli3117@gmail.com>
2023-10-24 11:19:52 +0300

    stock-api folder structure

---
+++ src/StockAPI/API.StockAPI/Controllers/StockController.cs
@@ -0,0 +1,38 @@
+﻿using API.StockAPI.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.StockAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StockController : ControllerBase
+    {
+        StockServices _services = new();
+
+        public StockController(StockServices services)
+        {
+            _services = services;
+        }
+
+        [HttpGet]
+        [Route("Stock/Current/{symbol}")]
+        public async Task<IActionResult> GetCurrentStock(string symbol)
+        {
+            if (symbol == null)
+            {
+                return BadRequest();
+            }
+
+            string function = "TIME_SERIES_INTRADAY";
+            var result = await _services.GetCurrentStock(symbol, function);
+
+            if (result == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(result);
+        }
+    }
+}
---
+++ src/StockAPI/API.StockAPI/Services/StockServices.cs
@@ -0,0 +1,40 @@
+﻿using API.StockAPI.Models;
+using Newtonsoft.Json;
+
+namespace API.StockAPI.Services
+{
+    public class StockServices
+    {
+        private readonly string apiKey;
+
+        public StockServices()
+        {
+            apiKey = "IMMIS8YCRHPXCOJP";
+        }
+        public async Task<TimeSeriesData> GetCurrentStock(string symbol, string function)
+        {
+            string response = await JsonDataGenerator(symbol, function);
+            StockData? data = JsonConvert.DeserializeObject<StockData>(response);
+
+            var timeSeriesData = data.TimeSeries;
+            timeSeriesData.First().Value.Symbol = symbol;
+            timeSeriesData.First().Value.Date = timeSeriesData.First().Key;
+
+            var result = timeSeriesData.First().Value;
+            return result;
+        }
+
+        public async Task<string> JsonDataGenerator(string symbol, string function)
+        {
+            HttpClient _client = new HttpClient();
+
+            string _url = $"https://www.alphavantage.co/query?function={function}&symbol={symbol}&interval=60min&apikey={apiKey}";
+
+            HttpResponseMessage response = await _client.GetAsync(_url);
+
+            string json = await response.Content.ReadAsStringAsync();
+
+            return json;
+        }
+    }
+}
