fb652e1e3582dbfe29b15df3fc98a10c0515a3d2
1243557 <steli3117@gmail.com>
2023-10-30 22:21:42 +0200

    stock api refactor

---
+++ src/StockAPI/API.StockAPI.Domain/InterFaces/IStockService.cs
@@ -0,0 +1,20 @@
+﻿using API.StockAPI.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.StockAPI.Domain.InterFaces
+{
+    public interface IStockService
+    {
+        Task<TimeSeriesData> GetCurrentStock(string response, string symbol);
+
+        Task<TimeSeriesData> GetDailyStock(string response, string symbol);
+
+        Task<TimeSeriesData> GetWeeklyStock(string response, string symbol);
+
+        Task<TimeSeriesData> GetMonthlyStock(string response, string symbol);
+    }
+}
