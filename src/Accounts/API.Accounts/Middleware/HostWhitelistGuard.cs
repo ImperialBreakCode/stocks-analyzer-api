4b5d8111d5e8b9bb75f097d13b7196fc0830b06f
ImperialBreakCode <eeeda522@gmail.com>
2023-11-13 22:26:01 +0200

    reverting accounts

--- src/Accounts/API.Accounts/Middleware/HostWhitelistGuard.cs
+++ src/Accounts/API.Accounts/Middleware/HostWhitelistGuard.cs
@@ -14,7 +14,6 @@ namespace API.Accounts.Middleware
 
         public async Task InvokeAsync(HttpContext httpContext, IAccountsSettingsManager settingsManager)
         {
-            Console.WriteLine(httpContext.Connection.RemoteIpAddress);
             //string ip = $"{httpContext.Connection.RemoteIpAddress}:{httpContext.Connection.RemotePort}";
             string ip = $"{httpContext.Connection.RemoteIpAddress}";
 
