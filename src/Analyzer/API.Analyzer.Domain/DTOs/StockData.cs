97f919fd3439dcb94a8ef2318e9907b40dafb9e2
Diana <dayanatodorova14@gmail.com>
2023-11-21 15:17:39 +0200

    .

--- src/Analyzer/API.Analyzer.Domain/DTOs/StockData.cs
+++ src/Analyzer/API.Analyzer.Domain/DTOs/StockData.cs
@@ -1,19 +1,18 @@
-﻿using Newtonsoft.Json;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace API.StockAPI.Domain.Models
+namespace API.Analyzer.Domain.DTOs
 {
-    public class StockData
+    class StockData
     {
-        public string? Symbol { get; set; }
-        public string? Date { get; set; }
+        public string Symbol { get; set; }
+        public string Date { get; set; }
         public double Open { get; set; }
-        public double Low { get; set; }
         public double High { get; set; }
+        public double Low { get; set; }
         public double Close { get; set; }
         public int Volume { get; set; }
         public decimal NetIncome { get; set; }
--- src/Analyzer/API.Analyzer.Infrastructure/Services/ApiService.cs
+++ src/Analyzer/API.Analyzer.Infrastructure/Services/ApiService.cs
@@ -10,25 +10,31 @@ using API.Analyzer.Domain.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using API.Accounts.Domain.Entities;
 using API.Accounts.Domain.Interfaces.DbContext;
+using API.StockAPI.Domain.Models;
 
 namespace API.Analyzer.Infrastructure.Services
 {
-    public class ApiService : IApiService {
+    public class ApiService : IApiService 
+    {
 
-  
-        private readonly HttpClient httpClient;
+        private readonly HttpClient accountsClient;
+        private readonly HttpClient stocksClient;
 
         public ApiService()
         {
-            httpClient = new HttpClient();
-            httpClient.BaseAddress = new Uri("http://localhost:5001");
+            accountsClient = new HttpClient();
+            accountsClient.BaseAddress = new Uri("https://localhost:5032");
+
+            stocksClient = new HttpClient();
+            stocksClient.BaseAddress = new Uri("https://localhost:5031");
         }
 
+        //Connection with AccountsAPI
         public async Task<GetWalletResponseDTO> UserProfilInfo(string userId)
         {
             string getUrl = $"/api/accounts/{userId}";
 
-            HttpResponseMessage response = await httpClient.GetAsync(getUrl);
+            HttpResponseMessage response = await accountsClient.GetAsync(getUrl);
 
             if (response.IsSuccessStatusCode)
             {
@@ -42,11 +48,31 @@ namespace API.Analyzer.Infrastructure.Services
                 Console.WriteLine($"HTTP Error: {response.StatusCode}");
                 throw new Exception($"HTTP Error: {response.StatusCode}");
             }
-            
+
         }
-        public async Task<decimal?> ProfitablenessAccountCheck(string userId, decimal? balance)
+
+        //Connection with StockAPI
+        public async Task<StockData> GetStockInfo(string symbol)
         {
-            var userProfile = await UserProfilInfo(userId);
+            string getUrl = $"/api/stocks/{symbol}";
+
+            HttpResponseMessage response = await stocksClient.GetAsync(getUrl);
+            if (response.IsSuccessStatusCode)
+            {
+                string jsonContent = await response.Content.ReadAsStringAsync();
+                var result = JsonConvert.DeserializeObject<StockData>(jsonContent);
+                return result;
+            }
+            else
+            {
+                Console.WriteLine($"HTTP Error: {response.StatusCode}");
+                throw new Exception($"HTTP Error: {response.StatusCode}");
+            }
+        }
+
+        public async Task<decimal?> CurrentProfitability(string userName)
+        {
+            var userProfile = await UserProfilInfo(userName);
             if (userProfile != null)
             {
                 return userProfile.Balance;
@@ -54,38 +80,28 @@ namespace API.Analyzer.Infrastructure.Services
             return null;
         }
 
-        //    public decimal? GetProfitabilityForDate(string userName, DateTime date)
-        //    {
-        //        var result = 
-        //            .Where(data => data.UserName == userName && data.Date == date)
-        //            .SingleOrDefault();
+        public async Task<decimal> PercentageChange(string symbol)
+        {
+            try
+            {
+                StockData stockResponse = await GetStockInfo(symbol);
 
-        //        return result?.Profitability;
-        //    }
+                decimal currentNetIncome = stockResponse.NetIncome;
+                decimal currentCommonSharesOutstanding = stockResponse.CommonSharesOutstanding;
 
+                // Calculate Percentage Change
+                decimal percentageChange = (currentNetIncome / currentCommonSharesOutstanding) * 100;
 
-        //    public decimal? GetProfitabilityForToday(string userName)
-        //    {
-        //        DateTime today = DateTime.Today;
-        //        return GetProfitabilityForDate(userName, today);
-        //    }
-
-        //    public decimal? GetProfitabilityForYesterday(string userName)
-        //    {
-        //        DateTime yesterday = DateTime.Today.AddDays(-1);
-        //        return GetProfitabilityForDate(userName, yesterday);
-        //    }
-
-        //    private readonly IAccountsDbContext accountsDbContext;
-
-        //    public ApiService(IAccountsDbContext accountsDbContext)
-        //    {
-        //        this.accountsDbContext = accountsDbContext;
-        //    }
-
-       
-
+                return percentageChange;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error retrieving current stock percentage change: {ex.Message}");
+                throw;
+            }
+        }
 
     }
+
 }
 
